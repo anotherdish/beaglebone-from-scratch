e42ce8f7151afd5730ee0eddad206e27b44b9312
anotherdish <anotherdish@dish.network>
2023-04-27 16:56:57 -0400

    more

---
+++ part4_solution/main.s
@@ -0,0 +1,20 @@
+.section b 
+    .ascii "b"
+.section d
+    .ascii "d"
+.section c
+    .ascii "c"
+.section e
+    .ascii "e"
+.section a
+    .ascii "a"
+.section f
+    .ascii "f"
+.section .text
+    mov r0, #1
+    mov r1, #2
+    mov r2, #3
+.section .text2
+    mov r0, #1
+    mov r1, #2
+    mov r2, #3
